95bdb7a21eb43f2cadac92d6427ae13c06e23df1
webgeardarko <webgeardarko@gmail.com>
2017-03-01 21:18:44 -0800

    Big Update

---
+++ js/main.js
@@ -0,0 +1,102 @@
+function startAnimation() {
+    document.getElementById("back").style.animation = "dreams 10.5s infinite ease-in-out";
+}
+
+var vid = document.getElementById("bgvid");
+var pauseButton = document.querySelector("#polina button");
+
+function vidFade() {
+  vid.classList.add("stopfade");
+}
+
+vid.addEventListener('ended', function()
+{
+// only functional if "loop" is removed 
+vid.pause();
+// to capture IE10
+vidFade();
+}); 
+
+
+pauseButton.addEventListener("click", function() {
+  vid.classList.toggle("stopfade");
+  if (vid.paused) {
+    vid.play();
+    pauseButton.innerHTML = "Pause";
+  } else {
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+  }
+})
+
+function fx0() {
+   document.getElementById('vid1').style.display = "none";
+   document.getElementById('vid2').style.display = "none";
+    document.getElementById('vid3').style.display = "none";
+	 document.getElementById('vid4').style.display = "none";
+	  document.getElementById('vid5').style.display = "none";
+           document.getElementById('vid6').style.display = "none";
+}
+function fx1() {
+   document.getElementById('vid1').style.display = "block";
+   document.getElementById('vid2').style.display = "none";
+    document.getElementById('vid3').style.display = "none";
+	 document.getElementById('vid4').style.display = "none";
+	  document.getElementById('vid5').style.display = "none";
+           document.getElementById('vid6').style.display = "none";
+}
+function fx2() {
+   document.getElementById('vid1').style.display = "none";
+   document.getElementById('vid2').style.display = "block";
+    document.getElementById('vid3').style.display = "none";
+	 document.getElementById('vid4').style.display = "none";
+	  document.getElementById('vid5').style.display = "none";
+           document.getElementById('vid6').style.display = "none";
+}
+function fx3() {
+   document.getElementById('vid1').style.display = "none";
+   document.getElementById('vid2').style.display = "none";
+    document.getElementById('vid3').style.display = "block";
+	 document.getElementById('vid4').style.display = "none";
+	  document.getElementById('vid5').style.display = "none";
+           document.getElementById('vid6').style.display = "none";
+}
+function fx4() {
+   document.getElementById('vid1').style.display = "none";
+   document.getElementById('vid2').style.display = "none";
+    document.getElementById('vid3').style.display = "none";
+	 document.getElementById('vid4').style.display = "block";
+	  document.getElementById('vid5').style.display = "none";
+           document.getElementById('vid6').style.display = "none";
+}
+function fx5() {
+   document.getElementById('vid1').style.display = "none";
+   document.getElementById('vid2').style.display = "none";
+    document.getElementById('vid3').style.display = "none";
+	 document.getElementById('vid4').style.display = "none";
+	  document.getElementById('vid5').style.display = "block";
+           document.getElementById('vid6').style.display = "none";
+}
+function fx6() {
+   document.getElementById('vid1').style.display = "none";
+   document.getElementById('vid2').style.display = "none";
+    document.getElementById('vid3').style.display = "none";
+	 document.getElementById('vid4').style.display = "none";
+	  document.getElementById('vid5').style.display = "none";
+           document.getElementById('vid6').style.display = "block";
+}
+
+        function toggleState(item){
+           if(item.className == "on") {
+              item.className="off";
+           } else {
+              item.className="on";
+           }
+        }
+
+function hideDiv() {
+   document.getElementById('backgrounds').className = "off";
+   document.getElementById('anibackgrounds').className = "off";
+   document.getElementById('backsettings').className = "off";
+   document.getElementById('backfx').className = "off";
+}
