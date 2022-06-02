653db8bb0b0f61fc5c5efa3ab9b1e4ad0e430eff
webgeardarko <webgeardarko@gmail.com>
2022-06-02 08:11:45 -0700

    update

--- scripts/test2.js
+++ scripts/test2.js
@@ -1,12 +1,6 @@
-<script>
 var client = new XMLHttpRequest();
 client.open('GET', 'https://pastebin.com/raw/a8iGNU8S');
 client.onreadystatechange = function() {
   console.log(client.responseText);
 }
 client.send();
-</script>
-
-<script>
-client.responseText
-</script>
